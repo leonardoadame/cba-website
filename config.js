35838d9874a11c1e2c625a09e03e343374023c5a
naarrek <h.narek@gmail.com>
2020-05-24 02:37:01 +0400

    duplicate logic removed

--- config.js
+++ config.js
@@ -4,7 +4,19 @@ const templateData =
     title: "CBA",
     subtitle: "Chromium Browser Automation (extension for chrome browser automation)",
     menuItems: ["", "documentation", "tutorial", "ready-projects",
-      "what's-new"]
+      "what's-new"],
+    pagesList: function (absolutePath) {
+      optionsList = templateData.site.queryPages(({originalPathname}) => {
+        return originalPathname && originalPathname.split('/')[0] == absolutePath.split('/')[0] && 
+            originalPathname.split('/')[1] == absolutePath.split('/')[1]});
+      arr = [];
+      for (const {title, pathname, originalPathname} of optionsList) {
+        if (originalPathname !== absolutePath) {
+          arr.push(`<li><a href = '${pathname}'>${title}</a></li>`)
+        }
+      }
+      return `<ul>${arr.join('')}</ul>`;
+    }
   }
 };
 
