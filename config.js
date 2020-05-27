599ab3a5f6a13558b58a55c4471127fd4595d7f0
naarrek <h.narek@gmail.com>
2020-05-27 17:47:12 +0400

    duplicate logic solved

--- config.js
+++ config.js
@@ -5,17 +5,27 @@ const templateData =
     subtitle: "Chromium Browser Automation (extension for chrome browser automation)",
     menuItems: ["", "documentation", "tutorial", "ready-projects",
       "what's-new"],
-    pagesList: function (absolutePath) {
-      optionsList = templateData.site.queryPages(({originalPathname}) => {
-        return originalPathname && originalPathname.split('/')[0] == absolutePath.split('/')[0] && 
-            originalPathname.split('/')[1] == absolutePath.split('/')[1]});
-      arr = [];
-      for (const {title, pathname, originalPathname} of optionsList) {
-        if (originalPathname !== absolutePath) {
-          arr.push(`<li><a href = '${pathname}'>${title}</a></li>`)
-        }
-      }
-      return `<ul>${arr.join('')}</ul>`;
+    listOfPages: function (pathname) {
+      const query = templateData.site.queryPages;
+      const {originalPathname} = query((page) => page.pathname === pathname)[0];
+            
+      pagesQuery = query((page) => page.originalPathname.startsWith(originalPathname) &&
+        page.originalPathname.replace(originalPathname, "").split("/").length === 2);
+        
+      pagesQuery.sort((a, b) => {
+        if (!a.weight)
+          return 1;
+        if (!b.weight)
+          return -1;
+        return a.weight - b.weight;
+      });
+
+      const items = pagesQuery.map((item) => 
+        `<li><a href = '${item.pathname}'>${item.title}</a></li>`);
+      
+      const result = `<ul>${items.join("")}</ul>`;
+      
+      return result;
     }
   }
 };
