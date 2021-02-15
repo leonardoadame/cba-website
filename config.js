9be58a2777881557f6fa06de44580a15762e0e94
Manvel Saroyan <saroyanm@gmail.com>
2021-02-15 21:43:40 +0300

    No issue - Moved helper functions from the config.js into helpers.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const argv = require("minimist")(process.argv.slice(2));
+const {getSubPages, listOfPages, sortByWeight} = require("./helpers");
 
 const templateData =
 {
@@ -6,65 +7,7 @@ const templateData =
     title: "CBA",
     subtitle: "Chromium Browser Automation (extension for chrome browser automation)",
     menuItems: ["", "documentation", "tutorial", "ready-projects",
-      "releases"],
-    sortByWeight: (a, b) =>
-    {
-      if (!a.weight)
-        return 1;
-      if (!b.weight)
-        return -1;
-      return a.weight - b.weight;
-    },
-    listOfPages: function (pathname)
-    {
-      const query = templateData.site.queryPages;
-      const {originalPathname} = query((page) => page.pathname === pathname)[0];
-
-      const pagesQuery = query((page) => page.originalPathname.startsWith(originalPathname) &&
-        page.originalPathname.replace(originalPathname, "").split("/").length === 2);
-
-      pagesQuery.sort(templateData.site.sortByWeight);
-
-      const items = pagesQuery.map((item) =>
-        `<li><a href = '${item.pathname}'>${item.title}</a></li>`);
-
-      return `<ul>${items.join("")}</ul>`;
-    },
-    /**
-     * Get subpages according to the pages folder structure.
-     * @param {Object} page     Page instance from site.queryPages
-     * @param {Function} filter filter pages that should include subpages
-     * @return {Object} returns passed pages object with populated `sub_pages`
-     *                          properties sorted according to page weights
-     */
-    getSubPages: function (page, filter)
-    {
-      const query = templateData.site.queryPages;
-      const subPages = query(({originalPathname}) =>
-      {
-        const parentPath = `${page.originalPathname}/`;
-        return originalPathname.startsWith(parentPath) &&
-               !(originalPathname.substring(parentPath.length).includes("/"));
-      });
-
-      if (subPages.length)
-      {
-        page.sub_pages = [];
-        for (const subPage of subPages)
-        {
-          if (subPage.menu !== false)
-          {
-            page.sub_pages.push(subPage);
-            if (!filter || filter(subPage))
-            {
-              templateData.site.getSubPages(subPage, filter);
-            }
-          }
-        }
-        page.sub_pages.sort(templateData.site.sortByWeight);
-      }
-      return page;
-    }
+      "releases"]
   }
 };
 
@@ -80,5 +23,9 @@ if (!argv.dev)
   lessOptions.minify = true;
   jsModuleOptions.minify = {sourceMap: false}
 }
+
+templateData.site.listOfPages = listOfPages.bind(templateData.site);
+templateData.site.getSubPages = getSubPages.bind(templateData.site);
+templateData.site.sortByWeight = sortByWeight.bind(templateData.site);
 
 module.exports = { templateData, port, lessOptions, jsModuleOptions };
---
+++ helpers.js
@@ -0,0 +1,62 @@
+function listOfPages(pathname)
+{
+  const query = this.queryPages;
+  const {originalPathname} = query((page) => page.pathname === pathname)[0];
+
+  const pagesQuery = query((page) => page.originalPathname.startsWith(originalPathname) &&
+    page.originalPathname.replace(originalPathname, "").split("/").length === 2);
+
+  pagesQuery.sort(this.sortByWeight);
+
+  const items = pagesQuery.map((item) =>
+    `<li><a href = '${item.pathname}'>${item.title}</a></li>`);
+
+  return `<ul>${items.join("")}</ul>`;
+}
+
+/**
+ * Get subpages according to the pages folder structure.
+ * @param {Object} page     Page instance from site.queryPages
+ * @param {Function} filter filter pages that should include subpages
+ * @return {Object} returns passed pages object with populated `sub_pages`
+ *                          properties sorted according to page weights
+ */
+function getSubPages(page, filter)
+{
+  const query = this.queryPages;
+  const subPages = query(({originalPathname}) =>
+  {
+    const parentPath = `${page.originalPathname}/`;
+    return originalPathname.startsWith(parentPath) &&
+            !(originalPathname.substring(parentPath.length).includes("/"));
+  });
+
+  if (subPages.length)
+  {
+    page.sub_pages = [];
+    for (const subPage of subPages)
+    {
+      if (subPage.menu !== false)
+      {
+        page.sub_pages.push(subPage);
+        if (!filter || filter(subPage))
+        {
+          this.getSubPages(subPage, filter);
+        }
+      }
+    }
+    page.sub_pages.sort(this.sortByWeight);
+  }
+  return page;
+}
+
+function sortByWeight(a, b)
+{
+  if (!a.weight)
+    return 1;
+  if (!b.weight)
+    return -1;
+  return a.weight - b.weight;
+}
+
+module.exports = {listOfPages, getSubPages, sortByWeight};
